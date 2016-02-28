beb9c9f57e0d41714ad994dbfc474f6202a40ceb
DimiExperience <stefan.dimi91@gmail.com>
2016-02-28 14:22:56 -0500

    FirstCommit generate N number of lists sort all of them console.writeline them

---
+++ IntegerManipulationPractise/IntegerUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerManipulationPractise
+{
+    public class IntegerUtils
+    {
+        public static List<int> ListSorter(List<int> list)
+        {
+            var givenNumbers = list;
+
+            var sortedNumbers = new List<int> {givenNumbers[0]};
+            givenNumbers.RemoveAt(0);
+
+            foreach (var number in givenNumbers)
+            {
+                var foundNumber = false;
+                for (var i = sortedNumbers.Count; i > 0; i--)
+                {
+                    if (number > sortedNumbers[i - 1])
+                    {
+                        foundNumber = true;
+                        sortedNumbers.Insert(i, number);
+                        break;
+                    }
+                }
+                if (!foundNumber)
+                {
+                    sortedNumbers.Insert(0, number);
+                }
+            }
+            return sortedNumbers;
+        }
+
+        public static List<int> GetRandomNumbers()
+        {
+            var count = 10;
+            var random = new Random();
+            var randomNumbers = new List<int>();
+
+            for (var i = 0; i < count; i++)
+            {
+                int number;
+
+                do number = random.Next(100); while (randomNumbers.Contains(number));
+
+                randomNumbers.Add(number);
+            }
+
+            return randomNumbers;
+        }
+    }
+}
---
+++ IntegerManipulationPractise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntegerManipulationPractise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many lists?");
+            int numOfLists = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < numOfLists; i++)
+            {
+                Thread.Sleep(10); //HAHA, btw, morao sam da mu stavim sleep da ne bi ponovio liste:)
+
+                var randomList = new List<int>();
+                randomList.AddRange(IntegerUtils.GetRandomNumbers());
+                    //ZASTO MORAM DA PISEM IntegerUtils, zasto ne moze samo GetRandomNumbers()???????
+
+                Console.WriteLine("\nUnsorted list No.{0}: ", i+1);
+                foreach (var num in randomList)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine("\nSorted List No.{0}: ", i+1);
+
+                var sortedList = IntegerUtils.ListSorter(randomList);
+                foreach (var num in sortedList)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
