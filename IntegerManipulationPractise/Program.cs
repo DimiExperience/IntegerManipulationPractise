243ca2eddb396ced139b64ff59f70561f4ec3461
DimiExperience <stefan.dimi91@gmail.com>
2016-02-28 14:35:13 -0500

    setup for BIG O counting and time change

--- IntegerManipulationPractise/Program.cs
+++ IntegerManipulationPractise/Program.cs
@@ -11,11 +11,13 @@ namespace IntegerManipulationPractise
     {
         static void Main(string[] args)
         {
+            var startTime = DateTime.UtcNow;
+
             Console.WriteLine("How many lists?");
             int numOfLists = Convert.ToInt32(Console.ReadLine());
             for (var i = 0; i < numOfLists; i++)
             {
-                Thread.Sleep(10); //HAHA, btw, morao sam da mu stavim sleep da ne bi ponovio liste:)
+                Thread.Sleep(10); //HAHA, btw, morao sam da mu stavim sleep da ne bi ponovio liste:) mnogo mi je brz komp:P
 
                 var randomList = new List<int>();
                 randomList.AddRange(IntegerUtils.GetRandomNumbers());
@@ -29,11 +31,17 @@ namespace IntegerManipulationPractise
                 Console.WriteLine("\nSorted List No.{0}: ", i+1);
 
                 var sortedList = IntegerUtils.ListSorter(randomList);
+                    //ISTO PITANJE I OVDE, zasto ne moze bez IntegerUtils.?
+
                 foreach (var num in sortedList)
                 {
                     Console.Write(num + " ");
                 }
             }
+            var endTime = DateTime.UtcNow;//ne znam odakle da pocnem, ali sada cu da probam da izbrojim BIG O :)
+            var timeElapsed = endTime - startTime;
+            Console.WriteLine("\n" + timeElapsed);
+
         }
     }
 }
