dda744ce413a346e7afffa90baefb77fa38a55c2
DimiExperience <stefan.dimi91@gmail.com>
2016-03-15 15:17:22 -0400

    komentar...

--- IntegerManipulationPractise/Shuffle.cs
+++ IntegerManipulationPractise/Shuffle.cs
@@ -24,6 +24,11 @@ namespace IntegerManipulationPractise
 
         public static List<int> ShuffleListBackForth(List<int> list, int numOfShuffles)
         {
+            //ovaj shuffle treba da se bavi sa 0/1 tacnije da stavlja pre ili posle, 
+            //ali bih odradio da ima jos jedan random, koji ce da odredjuje koliko bi brojeva stavljao pre ili posle, 
+            //znaci 0/1 odredjuje da li sledeci broj stavlja pre ili posle proslog, a random od npr 1-5 ili tako nesto
+            //odredjuje koliko brojeva ce da stavlja, kao kada mesas karte... a na kraju, mogu da se povezu ova dva
+            //shuffle-a i da se napravi jedan koji radi sve, i totalno izmesa:)
             var retVal = new List<int>();
 
 
